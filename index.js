4f4b5666738647a7d0074d89f478ec0b021013e8
joahlan <162236545+joahlan@users.noreply.github.com>
2024-12-01 20:36:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require ("express");
+const sqlite3 = require ("sqlite3").verbose();
+const path = require("path");
+
+const db = new sqlite3.Database("Desenvolvimento.sqlite");
+
+const app = express();
+
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname, "public")))
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html")
+})
+
+
+
+app.listen(3000, console.log("rodando..."))
