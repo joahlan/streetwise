f3e64eb9da65e6570e172680cb06f795b54036a4
joahlan <162236545+joahlan@users.noreply.github.com>
2025-04-02 00:44:10 +0000

    carrinho

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require ("express");
-const sqlite3 = require ("sqlite3").verbose();
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 
 const app = express();
@@ -21,7 +21,7 @@ function Criar_tabela() {
     var sql = "CREATE TABLE USUARIOS(ID INTEGER PRIMARY KEY AUTOINCREMENT, USUARIO VARCHAR, EMAIL VARCHAR, SENHA VARCHAR, CPF VARCHAR, TELEFONE VARCHAR, CEP VARCHAR)";
 
     db.run(sql, err => {
-        if(err) console.log("deu erro");
+        if (err) console.log("deu erro");
         else console.log("deu certo");
     })
 };
@@ -32,9 +32,9 @@ app.post("/validar_cadastro", (req, res) => {
     console.log(req.body);
 
     var sql = "INSERT INTO USUARIOS(USUARIO, EMAIL, SENHA, CPF, TELEFONE, CEP) VALUES(?, ?, ?, ?, ?, ?)";
-    
+
     db.run(sql, [req.body.usuario, req.body.email, req.body.senha, req.body.cpf, req.body.telefone, req.body.cep], (err) => {
-        if(err) res.send(err);
+        if (err) res.send(err);
         else res.send("Cadastro Realizado");
     });
 
@@ -42,17 +42,24 @@ app.post("/validar_cadastro", (req, res) => {
 
 app.post("/validar_login", (req, res) => {
 
-    db.all("SELECT*FROM USUARIOS", (err, rows) => {
-        
-        for(var x = 0 ; x <= rows.length; x++){
-            if(usuario == rows.usuario[x && senha == rows.senha[x]])
-                console.log("Usuario já existe");
-            else console.log("Usuario incorreto")
+    db.all("SELECT COUNT(*) AS total FROM USUARIOS WHERE USUARIO = ? AND SENHA = ?", [req.body.usuario, req.body.senha], (err, rows) => {
+
+        if(err){
+            console.log("Usuario não existe") 
         }
 
-    })
+        if(rows.length > 0 && rows[0].total == 1){
 
-    res.send("ok")
+            console.log("Usuario encontrado")
+            return res.send("/index.html")
+            }
+
+        else{
+
+            console.log("Não existe esse usuario")
+            return res.send("/cadas.html")
+        }
+    });
 
 });
 
